7157b10299b40ced6be519dad1e9290a1a00c5bd
MyaGwen <wendy.e.ahadome@gmail.com>
2023-03-20 07:41:29 -0700

    coomit

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-// Get all the gallery items
+//! all the gallery items
 const galleryItems = document.querySelectorAll('.gallery-item');
 
-// Get the selected div
+//! selected div
 const selectedDiv = document.querySelector('.selected');
 
-// Get the hidden div
+//! hidden div
 const hiddenDiv = document.querySelector('.hidden');
 
-// Add a click event listener to each item
+//! click event listener for each item
 galleryItems.forEach(item => {
   item.addEventListener('click', () => {
-    // Get the clicked image source
+    //! clicked image source
     const clickedSrc = item.src;
 
-    // Change the image source in the selected div
+    //! Change the image source in the selected div
     selectedDiv.innerHTML = `<img src="${clickedSrc}" alt="Selected Image">`;
   });
-
-   // //  // Show the hidden div
-   // // selectedDiv.classList.remove('hidden');
  
 });
